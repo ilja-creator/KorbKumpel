3063495a70a4b8cc869a6399a61bd31da3a31291
Ilja <schnell.ilja@gmail.com>
2026-03-28 08:22:52 +0100

    Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const newListButton = document.getElementById("news_list");
+
+    newListButton.addEventListener("click", () => {
+        console.log("Button clicked!");
+    });
+});
