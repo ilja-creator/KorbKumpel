e25dfb8e720d46b27e49949caf31dbdec087b394
Ilja <schnell.ilja@gmail.com>
2026-06-17 19:01:43 +0200

    commit changes

--- korb_kumpel/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const newListButton = document.getElementById("news_list");
-
-    newListButton.addEventListener("click", () => {
-        console.log("Button clicked!");
-    });
-});
---
+++ list/script.js
@@ -0,0 +1,3 @@
+document.addEentListener("DOMConterLoaded", () => {
+    const name_list = document.getElementsById("list")
+})
